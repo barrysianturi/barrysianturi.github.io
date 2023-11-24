8d1fc83f74aed82834fa91f67462e56c0b4dac52
Barry Mikhael Cavin <barrys.sianturi@gmail.com>
2023-11-24 16:59:33 +0700

    Update script.js
    baca txt untuk list of nomor

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.getElementById('sendButton').addEventListener('click', function() {
-    var phone = document.getElementById('phone').value;
+    var fileInput = document.getElementById('fileInput');
     var message = document.getElementById('message').value;
 
-    if(phone && message) {
-        // Ini harus diganti dengan fungsi yang mengirim ke server backend
-        console.log('Pesan akan dikirim ke ' + phone + ': ' + message);
-        // Tampilkan QR Code (dummy)
-        document.getElementById('qrcode').innerText = 'QR Code akan muncul di sini';
-    } else {
-        alert('Nomor HP dan Pesan harus diisi!');
+    if(fileInput.files.length === 0) {
+        alert('Pilih file yang berisi daftar nomor!');
+        return;
     }
+
+    var file = fileInput.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+        var lines = e.target.result.split('\n');
+        for(var i = 0; i < lines.length; i++) {
+            var phone = lines[i].trim();
+            if(phone) {
+                // Proses kirim pesan ke nomor ini
+                console.log('Pesan akan dikirim ke ' + phone + ': ' + message);
+            }
+        }
+    };
+
+    reader.readAsText(file);
 });
